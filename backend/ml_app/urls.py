a99aedb042cc35739106839a546e1b9e8fc4005c
Harsh <harshtrivs@gmail.com>
2024-11-26 20:31:29 +0530

    Feat: Add Template form for prediction url
    - Create HTML template for prediction endpoint
    - Map variables to input of endpoint

--- backend/ml_app/urls.py
+++ backend/ml_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.shortcuts import render
 from . import views
 
 urlpatterns = [
     path("",views.home,name="home"),
-    path("predict",views.predict,name="predict")
+    path("predict",views.predict,name="predict"),
+    path('form', lambda request: render(request, 'predict.html'), name='predict_form'),
 ]
