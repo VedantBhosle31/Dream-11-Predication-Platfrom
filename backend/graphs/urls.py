fa15ba2cf7e1a05f281beba9a731a401df30df04
Blink29 <purukumar2905@gmail.com>
2024-11-27 21:00:57 +0530

    created endpoint for player radar chart

--- backend/graphs/urls.py
+++ backend/graphs/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("",views.hello,name="hello"),
-    path("get_player_points",views.get_player_points,name="get_player_points"),
+    path("get_player_points/<int:player_id>/",views.get_player_points,name="get_player_points"),
+    path("get_player_radar_chart/<int:player_id>/",views.get_player_radar_chart,name="get_player_radar_chart"),
 ]
