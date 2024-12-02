44a1128a74281fc703bf35d5b1f5eae754ceb147
Atharv Shete <atharvshete@hotmail.com>
2024-12-03 04:46:40 +0530

    update requirement.txt

--- backend/genai/views.py
+++ backend/genai/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hello, World!")
+
+def explain_graph(request):
+    if request.method == "POST":
+        graph_name = request.POST.get("graph_name")
+        player_id = request.POST.get("player_id")
+        
+        
